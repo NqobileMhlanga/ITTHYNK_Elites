e19699df4a015ba29065e3548f18270cca88d73a
NqobileMhlanga <mhlanganqobsile@gmail.com>
2021-11-22 21:40:07 +0200

    ninja movement code latest

--- code/NinjaMovement.cs
+++ code/NinjaMovement.cs
@@ -17,7 +17,18 @@ public class NinjaMovement : MonoBehaviour
 
     private void  Update()
     {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, body.velocity.y);
+
+        //flip direction of ninja when moving
+        if(horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+
+        }else if(horizontalInput < -0.01f){
+            transform.localScale = new Vector3(-1,1,1);
+        }
 
         if(Input.GetKey(KeyCode.Space))
         {
